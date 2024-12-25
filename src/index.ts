3395d91c24b0c6aa04a329096e172c7fba880215
Matt Ballard <mattjballard81@gmail.com>
2024-12-25 10:56:55 -0500

    further refactor User class to use generic Model and Collection classes for greater reusability

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { User } from "./models/User";
 
-const user = new User({ id: 1, age: 14 });
+const collection = User.buildUserCollection();
 
-user.on("save", () => {
-  console.log(user);
+collection.on("change", () => {
+  console.log(collection);
 });
 
-user.save();
+collection.fetch();
